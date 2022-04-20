2755cc199f9e71cd90dec05a89418dcdf4765718
Robin Monachan <2005@tkmce.ac.in>
2022-04-20 16:16:18 +0530

    File Upload

--- upgrade_application/src/main/java/com/upgrader/upgrade_application/service/FileUploadService.java
+++ upgrade_application/src/main/java/com/upgrader/upgrade_application/service/FileUploadService.java
@@ -5,31 +5,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
 	private static final Logger logger = LogManager.getLogger(FileUploadService.class);
-	
-	@Value("${configFilepath}")
-	private String propertyPath;
+	@Autowired
+	private GridFsTemplate template;
 
-	public void uploadFile(MultipartFile file) {
-		try {
-			File f = new File(file.getOriginalFilename());
-			Path newFilePath = Paths.get(propertyPath + f);
-			Files.createFile(newFilePath);
-			file.transferTo(f);
-		} catch (IllegalStateException e) {
-			logger.info("IllegalStateException occured" + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			logger.info("IOException occured" + e.getMessage());
-			e.printStackTrace();
-		}
+
+	public String uploadFile(MultipartFile upload) throws IOException {
+		//define additional metadata
+		DBObject metadata = new BasicDBObject();
+		metadata.put("fileSize", upload.getSize());
+
+		//store in database which returns the objectID
+		Object fileID = template.store(upload.getInputStream(), upload.getOriginalFilename(), upload.getContentType(), metadata);
+
+		//return as a string
+		return fileID.toString();
 	}
 }
